fcc7ce1cc0a8581a7e6e0c196b74054da5a07996
KilianOlry <kilian.olry@gmail.com>
2024-12-18 16:55:37 +0100

    /refactor/ Generate getter & setter

--- src/main/java/biblio/biblio/entities/Book.java
+++ src/main/java/biblio/biblio/entities/Book.java
@@ -15,4 +15,28 @@ public class Book {
 
     @Column(name = "synopsis", columnDefinition = "TEXT", nullable = false)
     private String synopsis;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
 }
