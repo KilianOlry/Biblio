6376bc054fdc6ca0e7059eecd9db856ef8b49d92
KilianOlry <kilian.olry@gmail.com>
2024-12-18 17:13:17 +0100

    /feat/ Add reset button on form & create gender entity

---
+++ src/main/java/biblio/biblio/entities/Gender.java
@@ -0,0 +1,44 @@
+package biblio.biblio.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "gender")
+
+public class Gender {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "keywords", nullable = true, columnDefinition = "TEXT")
+    private String keywords;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+}
